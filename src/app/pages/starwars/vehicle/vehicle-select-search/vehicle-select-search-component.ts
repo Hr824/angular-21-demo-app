a7c420b1b2196d019e34d2b432bb53e6ba7c3b65
Hr824 <bdv.pro@gmail.com>
2026-04-24 15:29:57 +0200

    httpClient with Observable for Starwars vehicle select and search

--- src/app/pages/starwars/vehicle/vehicle-select-search/vehicle-select-search-component.ts
+++ src/app/pages/starwars/vehicle/vehicle-select-search/vehicle-select-search-component.ts
@@ -1,13 +1,15 @@
 import { Component, computed, inject, signal } from '@angular/core';
 import { VehicleService } from '../../../../services/starwars/vehicle-service';
 import { Vehicle } from '../../../../models/starwars/vehicle';
-import { JsonPipe, UpperCasePipe } from '@angular/common';
+import { AsyncPipe, JsonPipe, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpErrorResponse } from '@angular/common/http';
+import { catchError, of, switchMap } from 'rxjs';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-vehicle-select-search-component',
-  imports: [FormsModule, JsonPipe, UpperCasePipe],
+  imports: [FormsModule, JsonPipe, UpperCasePipe, AsyncPipe],
   templateUrl: './vehicle-select-search-component.html',
   styleUrl: './vehicle-select-search-component.css',
 })
@@ -15,7 +17,7 @@ export class VehicleSelectSearchComponent {
 
   vehicleService = inject(VehicleService);
 
-   vehicleModels = signal<string[]>([
+  vehicleModels = signal<string[]>([
     'landspeeder', 'airspeeder', 'bomber', 'transport', 'crawler', 'skyhopper', 'starfighter', 'barge', 'novehicle'
   ]);
 
@@ -31,4 +33,29 @@ export class VehicleSelectSearchComponent {
   isLoading = computed(() => this.vehicleHttpResource.isLoading());
 
 
+  //===========================================
+  //Avec httpClient et Observable
+  //===========================================
+  //OK le refresh fonctionne avec la dropdown
+  //https://angular.fr/signals/to-signal
+  //https://rxjs.dev/api/operators/switchMap
+  // private httpClient = inject(HttpClient);
+  // private vehicleUrl: string = 'https://swapi.py4e.com/api/vehicles';
+  vehicles = toSignal(
+    toObservable(this.selectedModel).pipe(
+      switchMap(() => {
+        if(this.selectedModel() === ''){
+          return of<Vehicle[]>([]);
+        }
+        else{
+          // this.httpClient.get<VehicleResponse>(`${this.vehicleUrl}/?search=${model}`).pipe(
+          //   map(response => response.results)
+          // )
+          return this.vehicleService.searchAllByModelWithHttpClient(this.selectedModel);
+        }
+      }),
+      catchError(() => of<Vehicle[]>([]))
+    ),
+    { initialValue: [] }
+  );
 }
--- src/app/services/starwars/vehicle-service.ts
+++ src/app/services/starwars/vehicle-service.ts
@@ -1,6 +1,7 @@
-import { httpResource } from '@angular/common/http';
-import { Injectable, Signal } from '@angular/core';
-import { VehicleResponse } from '../../models/starwars/vehicle';
+import { HttpClient, httpResource } from '@angular/common/http';
+import { inject, Injectable, Signal } from '@angular/core';
+import { Vehicle, VehicleResponse } from '../../models/starwars/vehicle';
+import { catchError, map, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -21,5 +22,28 @@ export class VehicleService {
   //Retourner 'undefined' permet de ne pas appeler l'API au chargement de la page
   searchAllVehicleByModel(vehicleModel: Signal<string>) {
     return httpResource<VehicleResponse>(() => vehicleModel() ? `${this.vehicleUrl}/?search=${vehicleModel()}` : undefined);
+  }
+
+  //===========================================
+  //Avec httpClient et Observable
+  //===========================================
+  httpClient = inject(HttpClient);
+  searchAllWithHttpClient(): Observable<Vehicle[]> {
+    const vehicles$ = this.httpClient.get<VehicleResponse>(this.vehicleUrl).pipe(
+      map(response => response.results),
+      catchError(()=> of<Vehicle[]>([])) // En cas d'erreur, retourne un tableau vide
+    );
+
+    return vehicles$;
+  }
+
+
+  searchAllByModelWithHttpClient(vehicleModel: Signal<string>): Observable<Vehicle[]> {
+    const vehicles$ = this.httpClient.get<VehicleResponse>(`${this.vehicleUrl}/?search=${vehicleModel()}`).pipe(
+      map(response => response.results),
+      catchError(()=> of<Vehicle[]>([])) // En cas d'erreur, retourne un tableau vide
+    );
+
+    return vehicles$;
   }
 }
